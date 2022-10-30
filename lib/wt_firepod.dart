980cd416c88c670240c5c6750bf66275a4c3f37d
Tim McMaster <tim@mcmaster.id.au>
2022-10-31 00:11:35 +1100

    added exports to the package

--- lib/wt_firepod.dart
+++ lib/wt_firepod.dart
@@ -1,7 +1,17 @@
 library wt_firepod;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/database_provider.dart';
+export 'src/firebase_init.dart';
+export 'src/firebase_providers.dart';
+export 'src/firebase_setup.dart';
+export 'src/firepod.dart';
+export 'src/firepod_init.dart';
+export 'src/firepod_map.dart';
+export 'src/firepod_map_int.dart';
+export 'src/firepod_map_object.dart';
+export 'src/firepod_map_string.dart';
+export 'src/firepod_object.dart';
+export 'src/firepod_providers.dart';
+export 'src/firepod_settings.dart';
+export 'src/generic_lookup_map.dart';
+export 'src/generic_lookup_map_notifier.dart';
