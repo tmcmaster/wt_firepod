b887a3dfc87eee27808feb67a7c1ec9fbba5d8ad
Tim McMaster <tim@mcmaster.id.au>
2023-09-09 23:07:47 +1000

    added providers for firebase storage, firestore and functions

--- lib/src/providers/firebase_providers.dart
+++ lib/src/providers/firebase_providers.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wt_firepod/src/auth/auth.dart';
 
@@ -31,5 +34,21 @@ mixin FirebaseProviders {
   static final database = Provider<FirebaseDatabase>(
     name: 'FirebaseDatabase',
     (ref) => throw Exception('Need to override the FirebaseDatabase provider.'),
+  );
+
+  static final firestore = Provider<FirebaseFirestore>(
+    name: 'FirebaseFirestore',
+    (ref) =>
+        throw Exception('Need to override the FirebaseFirestore provider.'),
+  );
+
+  static final storage = Provider<FirebaseStorage>(
+    name: 'FirebaseStorage',
+    (ref) => throw Exception('Need to override the FirebaseStorage provider.'),
+  );
+  static final functions = Provider<FirebaseFunctions>(
+    name: 'FirebaseFunctions',
+    (ref) =>
+        throw Exception('Need to override the FirebaseFunctions provider.'),
   );
 }
