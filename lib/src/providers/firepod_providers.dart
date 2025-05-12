37d5636495bdad92d338d1d6135aff5d94d9ad9e
Tim McMaster <tim@mcmaster.id.au>
2025-05-12 23:57:20 +1000

    fix path to user sites in the database, branching to dev to not effect wix_admin.

--- lib/src/providers/firepod_providers.dart
+++ lib/src/providers/firepod_providers.dart
@@ -4,7 +4,7 @@ import 'package:wt_firepod/src/providers/lists/firepod_list_object.dart';
 mixin FirepodProviders {
   static final sitesList = FirepodListObject<Site>(
     name: 'Site List',
-    path: '/data/{user}/sites',
+    path: '/v4/user/{user}/sites',
     decoder: Site.convert.from.dynamicMap.to.model,
     encoder: Site.convert.to.dynamicMap.from.model,
     idField: 'id',
